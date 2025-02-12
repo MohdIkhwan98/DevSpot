4c61e1b292ea19b329ec10c794087e1abbddfbb4
unknown <ikhwankhoshim98@gmail.com>
2025-02-12 11:51:56 +0800

    mvc with auth

---
+++ DevSpot/Controllers/JobPostingsController.cs
@@ -0,0 +1,57 @@
+﻿using DevSpot.Models;
+using DevSpot.Repositories;
+using DevSpot.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NuGet.Protocol.Core.Types;
+
+namespace DevSpot.Controllers
+{
+    [Authorize] //make all action class need to autorize to access
+    public class JobPostingsController : Controller
+    {
+        private readonly IRepository<JobPosting> _repository;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public JobPostingsController(IRepository<JobPosting> repository, UserManager<IdentityUser> userManager)
+        {
+            _repository = repository;
+            _userManager = userManager;
+        }
+        [AllowAnonymous] // overide [Authorize] to allow access from unauthenticate users
+        public async Task<IActionResult> Index()
+        {
+            var jobPostings = await _repository.GetAllAsync();
+            return View(jobPostings);
+        }
+
+        [Authorize(Roles = "Admin, Employer")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Admin, Employer")]
+        public async Task<IActionResult> Create(JobPostingViewModel jobPostingVm)
+        {
+            if (ModelState.IsValid)
+            {
+                var jobPosting = new JobPosting
+                {
+                    Title = jobPostingVm.Title,
+                    Description = jobPostingVm.Description,
+                    Company = jobPostingVm.Company,
+                    Location = jobPostingVm.Location,
+                    UserId = _userManager.GetUserId(User)
+                };
+                await  _repository.AddAsync(jobPosting);
+                
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(jobPostingVm);
+        }
+    }
+}
